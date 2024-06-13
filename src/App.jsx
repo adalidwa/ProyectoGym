6a8f0e6eb6151b81eb69f66a7cc95fe79bf4a4f2
adalidwa <adalidjesusxd@gmail.com>
2024-06-13 13:40:06 -0400

    Ya se termino el componente de notificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Entrenadores from "./componentes/Entrenador/Entrenadores.jsx";
 import PerfilEntrenador from "./componentes/PerfilEntrenador/PerfilEntrenador.jsx";
 import BarraDeMenu from "./componentes/BarraDeMenu/BarraDeMenu.jsx";
@@ -7,23 +8,28 @@ import FondoImagen from "./componentes/img/ImagenFondo.png";
 import BarraNavegacion from "./componentes/BarraNavegacion/BarraNavegacion.jsx";
 import Navegacion from "./componentes/Router/Navegacion.jsx";
 import RutinaAsignada from "./componentes/rutinaAsignada/RutinaAsignada.jsx";
-import AsignarRutina from "./componentes/asignarRutina/AsignarRutina.jsx"
+import AsignarRutina from "./componentes/asignarRutina/AsignarRutina.jsx";
 import EjeciciosAsignar from "./componentes/asignarEjercicio/EjeciciosAsignar.jsx";
 import DietaAsignada from "./componentes/DietaAsignada /DietaAsignada.jsx";
 import AsignacionAlimentos from "./componentes/asignarAlimentos/AlimentosAsignar.jsx";
-import AsignarDieta from "./componentes/asignarDieta/AsignarDieta.jsx"
+import AsignarDieta from "./componentes/asignarDieta/AsignarDieta.jsx";
 import "./App.css";
 import DietaAsignadaCliente from "./componentes/dietaasignadacliente/DietaAsignadaCliente.jsx";
+import Notificacion from "./componentes/Notificacion/Notificacion.jsx";
 const App = () => {
+  
   return (
     <div className="cuadro">
+      <Notificacion/>
       <DietaAsignadaCliente/>
       {/* <AsignarDieta/> */}
       {/* <AsignacionAlimentos/> */}
       {/* <DietaAsignada/> */}
       {/* <EjeciciosAsignar/> */}
       {/* <AsignarRutina/> */}
-     {/*  <RutinaAsignada/> */}
+      {/*  <RutinaAsignada/> */}
+      {/* <ToastContainer /> */}
+      
     </div>
   );
 };
